aca5bafca2229ff602002121de84bb7d31dd4469
King-Lota <lottanna47@gmail.com>
2026-02-16 14:07:31 +0100

    Enhance image loading function with error handling for network issues and invalid image formats

--- app/utils/image.py
+++ app/utils/image.py
@@ -4,5 +4,14 @@ from io import BytesIO
 
 
 def load_image(url: str):
-    response = requests.get(url)
-    return Image.open(BytesIO(response.content))
+    try:
+        response = requests.get(url,  timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Failed to download image: { str(e)}")
+
+    try:
+        return Image.open(BytesIO(response.content)).convert("RGB")
+    except Exception:
+        raise ValueError("Download file is not valid image")
+    #response = requests.get(url)

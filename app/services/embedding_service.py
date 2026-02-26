89e97263bc4c578c1f957ca9c54e202298a09684
King-Lota <lottanna47@gmail.com>
2026-02-26 12:44:02 +0100

    Fix import statement for vector store in embedding service and update upsert method call for consistency.

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,12 +1,13 @@
 import uuid
 
 from app.models.clip_model import clip_model
+from app.core.vector_store import vector_store
 
 def generate_embedding(crop):
     vector = clip_model.embed(crop)
     embedding_id = str(uuid.uuid4())
 
     # Vector DB insertion happens later
-    # vector_Store.upsert(embedding_id, vector)
+    vector_store.upsert(embedding_id, vector)
 
     return embedding_id
